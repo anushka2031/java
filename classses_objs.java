cda1d8a929ee24faa31d6b0d1c4ada8de2997277
anushka2031 <anushkatiwari2031@gmail.com>
2025-05-05 13:06:07 +0530

    5May

--- classses_objs.java
+++ classses_objs.java
@@ -1,8 +1,13 @@
 class Student{
-    String firstName="anushka";
-    String lastName="tiwari";
-    void fullName(){
-        System.out.println(firstName+ " " +lastName);
+    // String firstName="anushka";
+    // String lastName="tiwari";
+    // void fullName(){
+    //     System.out.println(firstName+ " " +lastName);
+    // }
+    String name="ravi";
+    Student(String name){
+        //name=firstName;
+        System.out.println(name);
     }
 }
 
@@ -18,9 +23,10 @@ class rectangle {
 public class classses_objs {
 
     public static void main(String[] args) {
-        Student obj = new Student();
-        obj.fullName();
-
+        Student obj = new Student("rahul");
+        //obj.fullName();
+        System.out.println(obj.name);
+       
         
 
         rectangle r = new rectangle();
