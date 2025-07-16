4e786b145a704d9e041ee4fe56782b3e6db5b416
anushka2031 <anushkatiwari2031@gmail.com>
2025-07-16 21:31:04 +0530

    16July

--- sortName.java
+++ sortName.java
@@ -22,7 +22,7 @@ class Student {
 }
 
 class SortbyName implements Comparator<Student>{
-    @Override
+   
     public int compare(Student s1, Student s2) {
         return s1.name.compareTo(s2.name);
     }
