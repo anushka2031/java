f1eeafdb106f784f58dae6353289a583e383ea7d
anushka2031 <anushkatiwari2031@gmail.com>
2025-07-30 20:03:18 +0530

    30July

--- string_builder.java
+++ string_builder.java
@@ -3,8 +3,11 @@ public class string_builder {
     public static void main(String[] args) {
 
         StringBuilder str=new StringBuilder(10);
+
         str.append("hello hii byee");
+
         System.out.println(str.length());
+        
         System.out.println(str.capacity());
         
     }
