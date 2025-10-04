6b178a5d9d61e95a8ec2ef60a10b960a18a84dda
anushka2031 <anushkatiwari2031@gmail.com>
2025-10-04 15:29:22 +0530

    4Sep

--- pattern.java
+++ pattern.java
@@ -16,43 +16,153 @@ public class pattern {
             System.out.println();
         } */
 
-      /*  for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(" ");
-            }
-            for(int k=5;k>=i;k--){
-                System.out.print("*");
-            }
-            System.out.println();
-        }*/
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=5;k>=i;k--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
 
-         for(int i=1;i<=5;i++){
-            for(int j=5;j>=i;j--){
-                System.out.print(" ");
-            }
-            for(int k=1;k<=i;k++){
-                System.out.print("*");
-            }
-            for(int p=1;p<i;p++){
-                System.out.print("*");
-            }
-            System.out.println();
-        } 
+        //  for(int i=1;i<=5;i++){
+        //     for(int j=5;j>=i;j--){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=1;k<=i;k++){
+        //         System.out.print("*");
+        //     }
+        //     for(int p=1;p<i;p++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // } 
 
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(" ");
-            }
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(" ");
+        //     }
             
-            for(int k=5;k>=i;k--){
-                System.out.print("*");
-            }
-            for(int l=5;l>i;l--){
-                System.out.print("*");
-            }
+        //     for(int k=5;k>=i;k--){
+        //         System.out.print("*");
+        //     }
+        //     for(int l=5;l>i;l--){
+        //         System.out.print("*");
+        //     }
 
-            System.out.println();
-        } 
+        //     System.out.println();
+        // } 
+
+        // for(int i=0;i<5;i++){
+        //     for(int j=0;j<5;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=0;i<5;i++){
+        //     for(int j=0;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=0;i<5;i++){
+        //     for(int j=5;j>i;j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=5;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // for(int i=0;i<5;i++){
+        //     for(int j=5;j>=i;j--){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=0;k<=i;k++){
+        //         System.out.print("*");
+        //     }
+        //     for(int l=1;l<=i;l++){
+        //         System.out.print("*");
+        //     }
+        //    System.out.println();
+        // }
+
+        // for(int i=0;i<5;i++){
+        //     for(int j=0;j<i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=5;k>i;k--){
+        //         System.out.print("*");
+        //     }
+        //     for(int l=4;l>i;l--){
+        //         System.out.print("*");
+        //     }
+        //    System.out.println();
+        // }
+
+        // for(int i=0;i<5;i++){
+        //     for(int j=5;j>=i;j--){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=0;k<=i;k++){
+        //         System.out.print("*");
+        //     }
+        //     for(int l=1;l<=i;l++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+
+        // }
+        // for(int i=0;i<5;i++){
+        //     for(int j=0;j<i+2;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=5;k>i;k--){
+        //         System.out.print("*");
+        //     }
+        //     for(int l=4;l>i;l--){
+        //         System.out.print("*");
+        //     }
+        //    System.out.println();
+        // }
+
+        // for(int i=0;i<5;i++){
+        //     for(int j=0;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i=0;i<4;i++){
+        //     for(int j=4;j>i;j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        
 
 
     }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,7 +1,25 @@
-   String  str="prograaming is a fun and fun is prograaming";
-           String[] words = str.split(" ");
-           HashMap<String,Integer> map = new HashMap<>();
-           for (String i : words) {
-               map.put(i, map.getOrDefault(i, 0)+1);
-           }
-         
+for(int i=0;i<5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i;k++){
+                System.out.print("*");
+            }
+            for(int l=1;l<=i;l++){
+                System.out.print("*");
+            }
+           System.out.println();
+        }
+
+        for(int i=0;i<5;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int k=5;k>i;k--){
+                System.out.print("*");
+            }
+            for(int l=4;l>i;l--){
+                System.out.print("*");
+            }
+           System.out.println();
+        }
